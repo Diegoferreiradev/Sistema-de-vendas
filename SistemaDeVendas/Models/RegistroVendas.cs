fc97372fcac089803f6508059bc5b1430ac15432
Diegoferreiradev <diegoferreiradev@gmail.com>
2019-03-03 02:21:18 -0300

    Criação das Outras Classes e a Segunda Migration

---
+++ SistemaDeVendas/Models/RegistroVendas.cs
@@ -0,0 +1,29 @@
+﻿using SistemaDeVendas.Models.Enums;
+using System;
+
+namespace SistemaDeVendas.Models
+{
+    public class RegistroVendas
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public double Quantidade { get; set; }
+        public VendaStatus Status { get; set; }
+        public Vendedor Vendedor { get; set; }
+
+
+        public RegistroVendas()
+        {
+
+        }
+
+        public RegistroVendas(int id, DateTime data, double quantidade, VendaStatus status, Vendedor vendedor)
+        {
+            Id = id;
+            Data = data;
+            Status = status;
+            Vendedor = vendedor;
+        }
+
+    }
+}
---
+++ SistemaDeVendas/Models/Vendedor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SistemaDeVendas.Models
+{
+    public class Vendedor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public double BaseSalarial { get; set; }
+
+        public Departamento Departamento { get; set; }
+        public ICollection<RegistroVendas> Vendas { get; set; } = new List<RegistroVendas>();
+
+
+        public Vendedor()
+        {
+
+        }
+
+        public Vendedor(int id, string nome, string email, DateTime dataNascimento, double baseSalarial, Departamento departamento)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            DataNascimento = dataNascimento;
+            BaseSalarial = baseSalarial;
+            Departamento = departamento;
+        }
+
+
+        public void AddVendas(RegistroVendas rv)
+        {
+            Vendas.Add(rv);
+        }
+
+        public void RemoverVendas(RegistroVendas rv)
+        {
+            Vendas.Remove(rv);
+        }
+
+        public double TotalVendas(DateTime inicial, DateTime final)
+        {
+            return Vendas.Where(rv => rv.Data >= inicial && rv.Data <= final).Sum(rv => rv.Quantidade);
+        }
+    }
+}
