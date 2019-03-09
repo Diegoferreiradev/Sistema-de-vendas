2a9419623508381794912db4b2d937d8110be779
Diegoferreiradev <diegoferreiradev@gmail.com>
2019-03-09 18:42:05 -0300

    Tela de Detalhes e o Eager loading

--- SistemaDeVendas/Services/VendedorService.cs
+++ SistemaDeVendas/Services/VendedorService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SistemaDeVendas.Services
 {
@@ -28,7 +29,7 @@ namespace SistemaDeVendas.Services
 
         public Vendedor FindById(int id)
         {
-            return _context.Vendedor.FirstOrDefault(obj => obj.Id == id);
+            return _context.Vendedor.Include(obj => obj.Departamento).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
